b5ab7d8ebef7ae81ee1c5e459f715a11eebc716c
macaveiAna <83480446+macaveiAna@users.noreply.github.com>
2023-11-03 18:03:53 -0600

    progress

--- src/lib.rs
+++ src/lib.rs
@@ -3,24 +3,23 @@
 //! Ana Macavei 2023
 
 //global constants
-const WIDTH:usize = 5;
-const HEIGHT:usize = 4;
+const MAX_WIDTH: usize = 5;
+const MAX_HEIGHT: usize = 4;
 
 // definition of struct Board
 pub struct Board {
     // A 2D array of booleans with dimensions 4x5
-    grid: [[bool; WIDTH]; HEIGHT],
+    grid: [[bool; MAX_WIDTH]; MAX_HEIGHT],
     width: usize,
     height: usize,
 }
 
 // Board type should support the following operations via impl
 impl Board {
-    pub fn create_board() -> Self {
+    pub fn create_board(width: usize, height: usize) -> Self {
         // Initialize the board to true because it would be false if eaten.
-        let grid = [[true; WIDTH]; HEIGHT];
-        let width = WIDTH;
-        let height = HEIGHT;
+        
+        let grid = [[true; MAX_WIDTH]; MAX_HEIGHT];
 
         Self {
             grid,
@@ -45,19 +44,34 @@ impl Board {
 
     // Chomp a given square, removing all squares below it and to the right of it
     // where width represents the rows and height represents the column
-    pub fn chomp_effect(board: &Board, row:usize, col:usize) {
-        for i in 0..row {
-            for j in 0..col {
-                
+    pub fn chomp_effect(board: &mut Board, row: usize, col: usize) {
+        // Loop through setting the users x's to false aka eaten
+        for i in row..board.height {
+            for j in col..board.width {
+                board.grid[i][j] = false;
             }
         }
-
     }
 
     // The negamax algorithm solves any zero-sum perfect-information
     // two-player game (like Chomp). It takes as input a board state and
     // outputs a winning move, if one exists.
     pub fn negamax(/*grid: Vec<Vec<bool>>*/) {
-        //call self.clone() here 
+        //call self.clone() here
     }
 }
+
+#[test]
+fn test_create_board() {
+    
+}
+
+fn test_chomp_effect() {
+    
+}
+
+fn test_negamax() {
+
+}
+
+
--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,19 @@ use chomp_game::Board;
 use prompted::input;
 fn main() {
 
-    let board = Board::create_board(); // testing to see if I can create the board successfully
+    println!("First, let's create the board!");
+
+    let width:usize= input!("Enter a width: ").trim().parse().unwrap();
+    let height:usize=input!("Enter a height: ").trim().parse().unwrap();
+    // Testing to see if I can create the board successfully
+    let mut board = Board::create_board(width, height); 
     Board::display_board(&board);
-    // Ask user to chomp using prompted
-    let row = input!("Enter the row: ");
-    let col = input!("Enter the column: ");
+    // Ask user to chomp using prompted input method
+    let row: usize = input!("Enter the row: ").trim().parse().unwrap();
+    let col: usize = input!("Enter the column: ").trim().parse().unwrap();
+
+    Board::chomp_effect(&mut board, row, col);
+    Board::display_board(&board);
+    
+
 }
