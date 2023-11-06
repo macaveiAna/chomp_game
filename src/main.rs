0171a74fbb0ac74ecae4f9e9da1fda043a3601fe
macaveiAna <83480446+macaveiAna@users.noreply.github.com>
2023-11-06 11:11:19 -0700

    added a while to fix unreachable code

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@
 //! Try to find a winning move. If there is one, perform it.
 //! Otherwise, stall by chomping as little as possible.
 //! (You can implement this by chomping the furthest-right piece in
-//!  the lowermost nonempty row.)
+//!  the lowermost nonempty row.) Might need to create a function
 //!
 //! add invalid input for create_board and chomp_effect (lib.rs)
 //!
@@ -21,29 +21,28 @@ use prompted::input;
 fn main() {
     println!("First, let's create the board!");
 
-    // + Add a condition here where if user enters a width > 5 and height > 4, asks user to try again
     let mut width: usize = input!("Enter a width: ").trim().parse().unwrap();
     let mut height: usize = input!("Enter a height: ").trim().parse().unwrap();
+
+    // Check for out of bounds when creating the board
+    while width > 5 && height > 4 {
+        println!("Out of bounds! Enter a board that has a width <= 5 and a height <= 4.");
+        width = input!("Enter a width: ").trim().parse().unwrap();
+        height = input!("Enter a height: ").trim().parse().unwrap();
+    }
+    // Testing to see if I can create the board successfully
+    let mut board = Board::create_board(width, height);
+    println!(" ");
+    Board::display_board(&board);
+
     loop {
-        if width > 5 && height > 4 {
-            println!("Out of bounds! Enter a board that has a width <= 5 and a height <= 4.");
-            width = input!("Enter a width: ").trim().parse().unwrap();
-            height = input!("Enter a height: ").trim().parse().unwrap();
-        }
-        // Testing to see if I can create the board successfully
-        let mut board = Board::create_board(width, height);
-        println!(" ");
-        Board::display_board(&board);
-   // }
+        // Repeat the board here using a loop AND loop exits when encountering a
+        // (0,0) then it's game over. (create a game over function)
 
-    // Repeat the board here using a loop AND loop exits when encountering a
-    // (0,0) then it's game over. (create a game over function)
-    //loop {
-    // Ask user to chomp using prompted input method
-    let mut col: usize = input!("Enter the column: ").trim().parse().unwrap();
-    let mut row: usize = input!("Enter the row: ").trim().parse().unwrap();
+        // Ask user to chomp using prompted input method
+        let mut col: usize = input!("Enter the column: ").trim().parse().unwrap();
+        let mut row: usize = input!("Enter the row: ").trim().parse().unwrap();
 
-    //loop {
         // give out of bounds error if user enters row: <= 3 col: <=4 or row = 0 col = 0
         if row == 4 && col == 5 || row == 0 && col == 0 {
             println!("Out of bounds! Please enter a row < 3 and a column < 4");
@@ -56,17 +55,16 @@ fn main() {
             println!("Game Over!");
             break;
         }
-    //}
 
-    Board::chomp_effect(&mut board, row, col);
-    println!(" ");
-    Board::display_board(&board);
-    if let Some(winning_move) = Board::negamax(&board) {
-        // catch winning move and chomp it
-        Board::chomp_effect(&mut board, winning_move.0, winning_move.1);
-    }
-    println!(" ");
-    Board::display_board(&board);
-    //}
+        // add a condition here if the user enters r=0 and c=0 then it's game over
+        Board::chomp_effect(&mut board, row, col);
+        println!(" ");
+        Board::display_board(&board);
+        if let Some(winning_move) = Board::negamax(&board) {
+            // catch winning move and chomp it
+            Board::chomp_effect(&mut board, winning_move.0, winning_move.1);
+        }
+        println!(" ");
+        Board::display_board(&board);
     }
 }
